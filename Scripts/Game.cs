d03d95ce44d3cded36bd9dcfb4a4e610bc09c3e5
Kayden Williams <10789755@AD.UVU.EDU>
2016-11-02 11:51:41 -0600

    if statements

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -1,6 +1,7 @@
 using System;
 
-public class Game{
+public class Game
+{
     public static bool canPlay = true;
     public Game()
     {
@@ -8,27 +9,49 @@ public class Game{
         Health.message = "You are getting stronger.";
         Ammo.message = "You have more ammo.";
         Cave.StartMessage = "You have enterted a cave.";
-        UnderWater.objects = new String[]{"SeaWead", "Coral", "Fish", "shark" };
+        UnderWater.objects = new String[] { "SeaWead", "Coral", "Fish", "shark" };
     }
     //runs at the start of the game
-    public void Start(){
+    public void Start()
+    {
         Console.WriteLine("Please type in your name:");
         name = Console.ReadLine();
         Console.WriteLine("Your Player name is " + name);
         Cave.Enter();
-        while (Game.canPlay){
+        while (Game.canPlay)
+        {
             GameTimer();
             Play();
         }
         Console.WriteLine("You Died");
         Console.WriteLine("Game Over");
     }
-    private void Play (){
+    public string gameState;
+
+    private void Play()
+    {
+        Console.WriteLine("Play commands: play, end, help");
+        gameState = Console.ReadLine();
+        if (gameState == "end")
+        {
+            Console.WriteLine("Game Over");
+            Environment.Exit(0);
+        }
+        if (gameState == "help")
+        {
+            Console.WriteLine("What do you need help for? If you can't play this game, you have issues.");
+            Play();
+        }
+        if (gameState != "help" || gameState != "play" || gameState != "end")
+            Play();
+        {
+            Console.WriteLine(gameState + " is not a valid option.");
+        }
         Random randomNum = new Random();
         Cave.Encounter(randomNum.Next(0, Cave.objects.Length));
     }
-
-    public static void GameTimer (){
+    public static void GameTimer()
+    {
         System.Threading.Thread.Sleep(2000);
     }
     //game levels
@@ -41,9 +64,9 @@ public class Game{
     private WeaponBase Gun = new WeaponBase();
     private WeaponBase Rifle = new WeaponBase();
 
-    
+
     public string name;
-    private int score;
+    //private int score;
 
 }
 /*
